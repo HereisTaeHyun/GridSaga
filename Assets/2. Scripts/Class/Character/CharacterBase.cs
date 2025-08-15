7703536e3dfc5c183f0f450006c2dbb70ada6d5b
HereisTaeHyun <kth882500@gmail.com>
2025-08-15 13:31:27 +0900

    캐릭터 베이스 부모 메서드화

--- Assets/2. Scripts/Class/Character/CharacterBase.cs
+++ Assets/2. Scripts/Class/Character/CharacterBase.cs
@@ -2,6 +2,38 @@ using UnityEngine;
 
 public class CharacterBase : MonoBehaviour
 {
-    [SerializeField] private CharacterSO characterData;
+    [SerializeField] protected CharacterSO characterData;
     public CharacterSO CharacterData => characterData;
+    [SerializeField] protected GameObject ActiveSkill;
+    [SerializeField] protected GameObject PassiveSkill;
+
+    protected virtual void Init()
+    {
+
+    }
+
+    protected virtual void Attack()
+    {
+
+    }
+
+    protected virtual void UseActiveSkill()
+    {
+        
+    }
+
+    protected virtual void UsePassiveSkill()
+    {
+        
+    }
+
+    protected virtual void GetDamage()
+    {
+
+    }
+    
+    protected virtual void Die()
+    {
+        
+    }
 }
